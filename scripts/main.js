1c79298f078e210ca4c4608db02f61f7c1c218f0
Thorkil <schjodt.thorkil@gmail.com>
2022-05-13 14:59:39 +0200

    rm btn html add web elem

--- scripts/main.js
+++ scripts/main.js
@@ -6,39 +6,86 @@ const Player = (name, symbol) =>  {
 }
 
 const Board = (() => {
-  const emptyBoard = new Array(9);
+  let gameBoard;
+
+  const newBoard = () => {
+    gameBoard = Array(9);
+  }
+
+
 
   return {
-    emptyBoard
+    newBoard
+  }
+})();
+
+const webElements = (() => {
+
+  const _createButton = (buttonText) => {
+    const buttonSquare = document.getElementById('square7');
+    const button = document.createElement('button');
+    button.classList.add('button');
+    button.textContent = buttonText;
+    buttonSquare.appendChild(button);
+    return button;
+  }
+
+  const newGameElements = () => {
+    const button = _createButton('new game');
+    button.classList.add('newgame')
+    button.addEventListener('click', _removeNew);
+    button.addEventListener('click', _addUserInput);
+  }
+
+  const _removeNew = () => {
+    const gameTitle = document.querySelectorAll('.menu-text');
+    const button = document.querySelector('.button');
+
+    for (let h1 of gameTitle) {
+      h1.remove();
+    }
+
+    button.remove();
+  }
+
+  const removeInput = () => {
+    const button = document.querySelector('.button');
+    button.remove();
+
+    const input = document.querySelector('.get-name')
+    input.remove()
+  }
+
+  const _addUserInput = () => {
+    const gameBoard = document.querySelector('.gameBoard')
+
+    const userInput = document.createElement('input')
+    userInput.classList.add('get-name');
+    userInput.setAttribute('placeholder', 'enter playername')
+    gameBoard.appendChild(userInput);
+
+    const button = _createButton('confirm name');
+    button.classList.add('confirm');
+
+    button.addEventListener('click', removeInput)
+  }
+
+
+  return {
+    newGameElements
   }
 })();
 
 const Game = (() => {
+  webElements.newGameElements();
+  Board.newBoard();
+
   let players = []
   players.push(Player('p1', 'X'), Player('p2', 'O'));
 
 
-  const removeElements = () => {
-    const gameTitle = document.querySelectorAll('.menu-text');
-    const button = document.querySelector('.newgame');
 
-    // gameTitle.remove();
-    for (let h1 of gameTitle) {
-      h1.remove();
-    }
-    button.remove();
-  }
 
-  const addUserInput = () => {
-    const gameBoard = document.querySelector('.gameBoard')
-    const userInput = document.createElement('input')
-    userInput.classList.add('get-name');
-    gameBoard.appendChild(userInput);
-  }
-
-  const button = document.querySelector('.newgame');
-  button.addEventListener('click', removeElements);
-  button.addEventListener('click', addUserInput);
 
 
   return {
